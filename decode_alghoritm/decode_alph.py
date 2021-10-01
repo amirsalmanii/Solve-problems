cdbe44f932ee84b047a953e583618d7fd0614d1e
amir-salmani <salmaniamir13@gmail.com>
2021-10-01 20:44:11 +0330

    Add files via upload

---
+++ decode_alghoritm/decode_alph.py
@@ -0,0 +1,37 @@
+#decode_logic-->  first time number
+engAlph = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+engWhitespaces = ('!', '?', ',', '.', ' ', ';', '"', "'")
+
+try:
+    client_numbers = input('enter numbers like: 2,33,24 for decode ..: ').split(',')
+    client_numbers = [int(num) for num in client_numbers]
+except ValueError:
+    print('wrong format input')
+    exit()
+
+
+finall_res = []
+tmp = 1
+for clnum in client_numbers:
+    if tmp == 1 or tmp == 2:
+        res = clnum % 27
+    else:
+        res = clnum % 9
+    
+    if res != 0:
+        if tmp == 1:
+            finall_res.append(engAlph[res - 1].upper())  # upper case added
+        if tmp == 2:
+            finall_res.append(engAlph[res - 1])  # lower case added
+        if tmp == 3:
+            finall_res.append(engWhitespaces[res - 1])  #white space added
+    elif res == 0:
+        tmp += 1
+        if tmp == 4:
+            tmp = 1
+
+join_res = ''.join(finall_res)
+print(join_res)
+
+
+
---
+++ decode_alghoritm/readme.txt
@@ -0,0 +1,12 @@
+the first time you need convert number to eng alphabet with uppercase mode  ex : number % 27 ==> 3 --> C or number % 27 ==> 11 ---> K
+
+if number % 27 result is 0 you need change mode and convert numbers to eng alphabet with lover case but still mode with number with 27  number % 27 ==> 1 ---> a 
+
+
+and rety if number & 27 result is 0 you need change mode and convert numbers to punctuation symbol --> ('!', '?', ',', '.', ' ', ';', '"', "'")
+but now number need to mode 9   number % 9 ==> 2 --> ?  
+
+and this cycle is alive when done
+
+
+#sorry for my bad english ! 

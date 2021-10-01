162acb3643966aa891577895122b8eb7e42e2611
amir-salmani <salmaniamir13@gmail.com>
2021-10-01 20:46:26 +0330

    Update decode_alph.py

--- decode_alghoritm/decode_alph.py
+++ decode_alghoritm/decode_alph.py
@@ -1,4 +1,3 @@
-#decode_logic-->  first time number
 engAlph = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
 engWhitespaces = ('!', '?', ',', '.', ' ', ';', '"', "'")
 
